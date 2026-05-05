890b5d50e6d51299108fd1c0a91244497568afcf
MaiwormGB <biel.maiworm@gmail.com>
2026-05-05 10:26:31 -0300

    sistema de abas e edição de sistema de blur (incompleto)

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const borrar = document.getElementById("blur");
 const nomes = document.getElementById("nomes");
 const botao = document.getElementById("salvar");
 
-let pagina = 1;
 
 function borrarFundo(){
+
+    
 
     const displayAtual = borrar.style.display || window.getComputedStyle(borrar).display;
 
@@ -17,6 +18,7 @@ function borrarFundo(){
         borrar.style.display = "none";
 
     }
+
 
 
 };
@@ -41,18 +43,65 @@ botao.addEventListener("click", function () {
 
 });
 
+let pagina = 1;
+
 function loop() {
     
+    if (pagina > 6){
+
+        pagina = 1;
+
+    }else if (pagina <1){
+
+        pagina = 6;
+
+    };
+
+    const pericias = document.getElementById("pericias");
+    const combate = document.getElementById("combate");
+    const relvas = document.getElementById("relvas");
+    const inventario = document.getElementById("inventario");
+    const talentos = document.getElementById("talentos");
+    const info = document.getElementById("info");
+
+
     switch(pagina){
         case 1:
-            
-
+            info.style.display = "none";
+            pericias.style.display = "flex";
+            combate.style.display = "none";
+            break;
+        case 2:
+            pericias.style.display = "none";
+            combate.style.display = "flex";
+            relvas.style.display = "none";
+            break;
+        case 3:
+            combate.style.display = "none";
+            relvas.style.display = "flex";
+            inventario.style.display = "none";
+            break;
+        case 4:
+            relvas.style.display = "none";
+            inventario.style.display = "flex";
+            talentos.style.display = "none";
+            break;
+        case 5:
+            inventario.style.display = "none";
+            talentos.style.display = "flex";
+            info.style.display = "none";
+            break;
+        case 6:
+            talentos.style.display = "none";
+            info.style.display = "flex";
+            pericias.style.display = "none";
+            break;
 
     }
 
     setTimeout(loop, 100);
 }
 
-loop();
+loop(pagina);
 
 
